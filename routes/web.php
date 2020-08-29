8e7ce262e36657e5d8f5ec20ad87e963d205d1a6
saroj <sarosessmdr@gmail.com>
2020-08-29 20:56:39 +0545

    view

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('posts/{slug}','PostsController@show');
 
+Route::get('/', function(){
+    return view('layouts.index');
+});
+Route::get('/about', function(){
+    return view('pages.about');
+});
+
